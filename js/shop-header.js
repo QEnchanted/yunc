4d1bc3296d063ffeef09e4872cb6d540e04bdd8a
Lacrimosa <qin@qq.com>
2019-08-15 20:29:40 +0800

    商城头部的完善

---
+++ js/shop-header.js
@@ -0,0 +1,11 @@
+$(function(){
+    $.ajax({
+        url:"shop-header.html",
+        type:"get",
+        success:function(result){
+            console.log(result);
+            $(result).replaceAll("header");
+            $(`<link rel="stylesheet" href="css/shop-header.css">`).appendTo("head");
+        }
+    })
+})
